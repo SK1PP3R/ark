b29455f3958c7bdfebaa03e2f79d076bc56a2f0e
SK1PP3R <56693392+SK1PP3R@users.noreply.github.com>
2023-02-27 16:08:42 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 apt install net-tools -y 2>/dev/null
 cd /home/steam/ark 2>/dev/null
+rm -rf /etc/arkmanager/
 rsync -a etc/arkmanager/ /etc/arkmanager/
 rm -rf etc 2>/dev/null
 mkdir steamcmd 2>/dev/null
