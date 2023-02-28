8b4cf5d9db530dcb449644ff1be56f0d87309180
SK1PP3R <56693392+SK1PP3R@users.noreply.github.com>
2023-02-28 01:28:26 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@ apt install net-tools htop -y 2>/dev/null && echo "Update." >/dev/null
 cd /home/steam/ark 2>/dev/null && echo "Update.." >/dev/null
 rm -rf /etc/arkmanager/ && echo "Update..." >/dev/null
 rsync -a etc/arkmanager/ /etc/arkmanager/ && echo "Update...." >/dev/null
-rm -rf etc 2>/dev/null && echo "Update..." >/dev/null
+rm -rf /home/steam/etc 2>/dev/null && echo "Update..." >/dev/null
+rm -rf /home/steam/arkmanager.cfg 2>/dev/null && echo "Update..." >/dev/null
 mkdir steamcmd 2>/dev/null && echo "Update.." >/dev/null
 ./user.sh && echo "Update.." >/dev/null
 ./run.sh && echo "Update." >/dev/null
