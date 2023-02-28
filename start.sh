18eb4148a07a6ad54464f95ae671befdcae874be
SK1PP3R <56693392+SK1PP3R@users.noreply.github.com>
2023-02-28 01:28:46 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@ cd /home/steam/ark 2>/dev/null && echo "Update.." >/dev/null
 rm -rf /etc/arkmanager/ && echo "Update..." >/dev/null
 rsync -a etc/arkmanager/ /etc/arkmanager/ && echo "Update...." >/dev/null
 rm -rf /home/steam/etc 2>/dev/null && echo "Update..." >/dev/null
-rm -rf /home/steam/arkmanager.cfg 2>/dev/null && echo "Update..." >/dev/null
 mkdir steamcmd 2>/dev/null && echo "Update.." >/dev/null
 ./user.sh && echo "Update.." >/dev/null
 ./run.sh && echo "Update." >/dev/null
